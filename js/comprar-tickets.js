a7e64ed5a3e50b21f18d811f86506532e2f15878
Luma2001 <lumaqui2001@yahoo.com.ar>
2022-11-15 23:15:34 -0300

    Modificación de código de html, js y css. Corrección validación

--- js/comprar-tickets.js
+++ js/comprar-tickets.js
@@ -1,210 +1,213 @@
-// Llamamos elementos de HTML 
-const valorTicket= 200;
-const form= document.getElementById("formulario");
-const inputs= document.querySelectorAll("#formulario input");
-const nombre= document.querySelector("#nombre");
-const apellido= document.querySelector("#apellido");
-const mail= document.querySelector("#mail");
-const cantidadTickets= document.querySelector("#cantidadTickets");
-const categoria= document.querySelector("#categoriaSelect");
-const totalPago= document.querySelector("#totalPago");
-const btnBorrar= document.querySelector("#btnBorrar");
-const resumen= document.querySelector("#btnResumen");
 
 
-// Funcion para volver el formulario a 0
-function borrar(){
+'use strict';
+//_________________________________________________________________________________
+//Declaración de Variables:
+const TICKET=200;
+let cant;
+let categoria;
+let descuento;
+let totalVenta;
+
+//_________________________________________________________________________________
+//Captura de Datos ingresados por el usuario
+
+const formulario=document.getElementById("formulario");
+const inputs=document.querySelectorAll("#formulario input");
+const select=document.querySelector("#formulario select");
+
+
+function capturaDatos() {
+    const nombre=document.querySelector("#nombre").value;
+    const apellido=document.querySelector("#apellido").value;
+    const mail=document.querySelector("#mail").value;
+    console.log("Nombre: " + nombre + ", Apellido: " + apellido + ", Mail: " + mail + ".");
+}
+
+
+function capturaCompra() {
+    cant=document.querySelector("#cantidadTickets").value;
+    categoria=document.querySelector("#categoriaSelect").value;
+    console.log("Cantidad ingresada: "+ cant + ". Categoría seleccionada: "+ categoria+".");//monto cantidad y categoria mostradas por consola
+}
+
+
+
+//_________________________________________________________________________________
+//Validación Formulario
+const expresiones = {
+    nombre:/^[a-zA-zÀ-ÿ\s]{3,40}$/,  //valido con Letras, números, acentos, entre 3 y 40 caracteres
+    apellido:/^[a-zA-zÀ-ÿ\s]{3,40}$/,
+    mail:/^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+\.[a-zA-Z]+$/, 
+    cant: /^\d{1,12}$/,
+    categoria: /^\d{0,3}$/
+}
+
+const campos = {
+	nombre: false,
+	apellido: false,
+	mail: false,
+    
+}
+
+
+  
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "apellido":
+			validarCampo(expresiones.apellido, e.target, 'apellido');
+		break;
+		case "mail":
+			validarCampo(expresiones.mail, e.target, 'mail');
+		break;
+		
+		
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+
+  inputs.forEach((input)=>{
+    input.addEventListener("keyup", validarFormulario);
+    input.addEventListener("blur", validarFormulario);
+  })
+
+
+
+
+
+
+//_________________________________________________________________________________
+//Calculo de descuento
+
+function promo() {
+switch (categoria){
+    
+    case "1":
+        descuento=(TICKET*80/100);
+        break
+    case "2":
+        descuento=(TICKET*50/100);
+        break
+    case "3":
+        descuento=(TICKET*15/100);
+        break
+    default:
+        descuento=(TICKET*0/100);
+        
+}   
+
+console.log("descuento total: "+descuento); //valor de descuento mostrado por consola
+}
+
+
+
+
+
+//borrar todo (aplico eventListener): 
+
+const borrar=document.getElementById("btnBorrar");
+
+borrar.addEventListener("click", () =>{
+    document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+        icono.classList.remove('formulario__grupo-correcto');
+    })
+    document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+    document.getElementById('formulario__boton-exito').classList.remove('formulario__boton-exito-activo');
+
+
+    totalPagar.textContent="";
     nombre.value="";
     apellido.value="";
     mail.value="";
     cantidadTickets.value="";
-    categoria.value="";
-    totalPago.textContent="";
-}
+    categoriaSelect.value=""; 
+})
 
-// Funcion para calcular descuento 
-function calcularDescuento(){
-    switch (categoria.value) {
-        case "0":
-            return (valorTicket - (valorTicket * 0));
+//mostrar total a Pagar luego de hacer click en Resumen:
+
+const totalPagar = document.querySelector("#totalPago");
+const resumen =document.querySelector("#btnResumen");
+
+
+
+
+function calcularTotal() {
+        capturaCompra();
+        promo();
+        totalVenta=parseInt(cant)*(TICKET-descuento);
+        return totalVenta;
             
-        case "1":  
-            return(valorTicket - (valorTicket * 0.80));
-
-        case "2":  
-            return(valorTicket - (valorTicket * 0.50));
-
-        case "3":  
-            return(valorTicket - (valorTicket * 0.15));
- 
-        default:
-            return false;
-    }
 }
 
-// Funcion para calcular el total
-function calcularTotal(){
-    let total= 0;
-    total= calcularDescuento() * parseInt( cantidadTickets.value);
-    return total;
+function mostrarTotal() {
+    totalPagar.textContent = calcularTotal();
+  }
+
+
+
+resumen.addEventListener("click", (e)=>{
     
-}
-
-function mostrarTotal(){
-    totalPago.textContent= calcularTotal();
-}
-
-function validaFalla(input,msje) {
-    const formControl= input.parentElement;
-    const aviso= formControl.querySelector("p");
-    aviso.innerText= msje;
-    formControl.className= "formulario_control.falla-input_activo";
-}
-
-function validaOk(input,msje){
-    const formControl= input.parentElement;
-    const aviso= formControl.querySelector("p");
-    aviso.innerText= msje;
-    formControl.className="formulario_control.ok";
-}
-
-function validaCategoriaFalla(select,msje){
-    const formControl= select.parentElement;
-    const categ= formControl.querySelector("p");
-    categ.innerText=msje;
-    formControl.className= "formulario_control.falla-input_activo";
-    
-}
-
-function validaCategoriaOk(select,msje){
-    const formControl= select.parentElement;
-    const aviso= formControl.querySelector("p");
-    aviso.innerText= msje;
-    formControl.className="formulario_control.ok";
-}
-
-function validaError(button,msje){
-    const formControl= button.parentElement;
-    const error= formControl.querySelector("p");
-    error.innerText=msje;
-    formControl.className= "formulario_control.falla-input_activo";
-    
-}
-
-
-// Evento, cuando hago click en el boton me llama a una funcion
- btnBorrar.addEventListener("click",borrar); 
- inputs.forEach((input)=>{
-    input.addEventListener("keyup",validarCampos);
-    input.addEventListener("blur",validarCampos);
- });
-
- resumen.addEventListener("click", (e)=>{
     e.preventDefault();
-   /* if(campos.nombre==false||campos.apellido==false||campos.mail==false||campos.cantidadTickets==false||campos.categoria==false){
-        validaError(resumen,"Por favor completar formulario correctamente.");
-    } else{
-        mostrarTotal();
-    }*/
+    calcularTotal();
     mostrarTotal();
-}) // cierre boton resumen
 
+    if(campos.nombre && campos.apellido && campos.mail && !isNaN(totalVenta)) {
+		document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+		document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo')
+        }, 5000);
+        document.getElementById('formulario__boton-exito').classList.add('formulario__boton-exito-activo');
+        
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+        });
 
-// Validacion
-const validaEmail = (email) => {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);        
-}
+        } else {
+             
+        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+        totalPagar.textContent = "0";        
+		};
+	   
+});
 
-const campos={
-    nombre:false,
-    apellido:false,
-    mail:false,
-    cantidadTickets:false,
-    categoria:false,
-}
+/*
 
-//Creamos funcion validar campos
- function validarCampos() {
-   //capturar los valores ingresados por el usuario
-    const nombreValor= nombre.value;
-    const apellidoValor= apellido.value;
-    const mailValor= mail.value;
-    const cantidadValor= cantidadTickets.value;
-    const categoriaValor= categoria.value;
-   
-    // Validando campo nombre
-    if (!nombreValor) {
-    console.log("nombre vacio");
-    validaFalla(nombre,"campo vacio");
-    campos.nombre= false
-} else {
-    validaOk(nombre,"");
-    campos.nombre= true
-}// cierre nombre valor
+if(campos.nombre==false || campos.apellido==false || campos.mail==false || isNaN(totalVenta)){
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+        totalPagar.textContent = "0";
+	} else {
+        document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+		document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+        document.getElementById('formulario__boton-exito').classList.add('formulario__boton-exito-activo');
+        
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	}
 
-
- // Validando campo apellido
- if (!apellidoValor) {
-    console.log("apellido vacio");
-    validaFalla(apellido,"campo vacio");
-    campos.apellido= false
-} else {
-    validaOk(apellido,"");
-    campos.apellido= true
-}// cierre nombre valor
-
-
-// Validando campo mail
-if (!mailValor) {
-    console.log("mail vacio");
-    validaFalla(mail,"campo vacio");
-    campos.mail= false
- } else if(!validaEmail(mailValor)){
-    validaFalla(mail,"El mail no es valido");
-    campos.mail= false
-} else {
-    validaOk(mail,"");
-    campos.mail= true
-}// cierre mail valor 
-
-// Validando campo cantidad ticket
-if (!cantidadValor) {
-    console.log("cantidad vacio");
-    validaFalla(cantidadTickets,"campo vacio");
-    campos.cantidadTickets= false
-} else {
-    validaOk(cantidadTickets,"");
-    campos.cantidadTickets= true
-}// cierre cantidad valor
-
-// Validando campo categoria
-if (categoriaValor==="") {
-    console.log("categoria vacio");
-    validaCategoriaFalla(categoria,"Seleccione una categoria");
-    campos.categoria= false
-} else {
-    validaCategoriaOk(categoria,"");
-    campos.categoria= true
-}// cierre categoria valor
-
-
-
-
-
-    
-};// cierre de validar campo
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
